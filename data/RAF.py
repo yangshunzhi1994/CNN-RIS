25b1a813f3bb66c96e70a7a92b14f4c0d7502c51
yangshunzhi1994 <42691414+yangshunzhi1994@users.noreply.github.com>
2019-05-28 16:34:52 +0800

    Add files via upload

---
+++ data/RAF.py
@@ -0,0 +1,64 @@
+''' Fer2013 Dataset class'''
+
+from __future__ import print_function
+from PIL import Image
+import numpy as np
+import h5py
+import torch.utils.data as data
+
+class RAF(data.Dataset):
+    """`FER2013 Dataset.
+
+    Args:
+        train (bool, optional): If True, creates dataset from training set, otherwise
+            creates from test set.
+        transform (callable, optional): A function/transform that  takes in an PIL image
+            and returns a transformed version. E.g, ``transforms.RandomCrop``
+    """
+
+    def __init__(self, split='Training', transform=None):
+        self.transform = transform
+        self.split = split  # training set or test set
+        self.data = h5py.File('/home/ysz/Mask_RCNN/data/pytoch/Expression10/data/RAF_data.h5', 'r', driver='core')
+        # now load the picked numpy arrays
+        if self.split == 'Training':
+            self.train_data = self.data['train_data_pixel']
+            self.train_labels = self.data['train_data_label']
+            self.train_data = np.asarray(self.train_data)
+            self.train_data = self.train_data.reshape((12271, 48, 48, 3))
+
+        else:
+            self.PrivateTest_data = self.data['valid_data_pixel']
+            self.PrivateTest_labels = self.data['valid_data_label']
+            self.PrivateTest_data = np.asarray(self.PrivateTest_data)
+            self.PrivateTest_data = self.PrivateTest_data.reshape((3068, 48, 48, 3))
+
+    def __getitem__(self, index):
+        """
+        Args:
+            index (int): Index
+
+        Returns:
+            tuple: (image, target) where target is index of the target class.
+        """
+        if self.split == 'Training':
+            img, target = self.train_data[index], self.train_labels[index]
+            
+        else:
+            img, target = self.PrivateTest_data[index], self.PrivateTest_labels[index]
+
+        # doing this so that it is consistent with all other datasets
+        # to return a PIL Image
+#         img = img[:, :, np.newaxis]
+#         img = np.concatenate((img, img, img), axis=2)
+        img = Image.fromarray(img)
+        if self.transform is not None:
+            img = self.transform(img)
+        return img, target
+
+    def __len__(self):
+        if self.split == 'Training':
+            return len(self.train_data)
+        
+        else:
+            return len(self.PrivateTest_data)
