c7efb2165380f67f62c1b6c992265acffb28a715
yangshunzhi1994 <42691414+yangshunzhi1994@users.noreply.github.com>
2019-06-10 15:15:50 +0800

    Update pytorch2onn.py

--- pytorch2onn.py
+++ pytorch2onn.py
@@ -4,7 +4,7 @@ import torchvision
 from models import *
 
 dummy_input = Variable(torch.randn(1, 3, 44, 44))
-model = DenseNet()
+model = EdgeCNN()
 
 model.load_state_dict(torch.load("/home/ysz/Mask_RCNN/data/pytoch/Expression5/models/FER2013_DenseNet/PrivateTest_model.t7"), strict=False)
 
